f152655e9321503ad45fd91d23578b04ac758719
hunterdoyle672740-create <hunter.doyle672740@gmail.com>
2026-08-19 12:25:06 +1000

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
-my_sprite = sprites.create(img("""
+def on_overlap_tile(sprite, location):
+    info.set_score(120 - 2 * coconut_2)
+    game.game_over(True)
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.stair_large,
+    on_overlap_tile)
+
+coconut_2 = 0
+Player_1 = sprites.create(img("""
         . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . 2 2 . . . . . . .
-        . . . . . . . 2 2 . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
+        . . . . c c c c . . . . . . . .
+        . . c c 5 5 5 5 c c . . . . . .
+        . c 5 5 5 5 5 5 5 5 c . . . . .
+        c 5 5 5 5 5 1 f 5 5 5 c . . . .
+        c 5 5 5 5 5 f f 5 5 5 5 c . . .
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . .
+        c c b b 1 b 5 5 5 5 5 5 d c . .
+        c 5 3 3 3 5 5 5 5 5 d d d c . .
+        . b 5 5 5 5 5 5 5 5 d d d c . .
+        . . c b b c 5 5 b d d d d c c .
+        . c b b c 5 5 b b d d d d c d c
+        . c c c c c c d d d d d d d d c
+        . . . c c c c d 5 5 b d d d c .
+        . . c c c c c b 5 5 b c c c . .
+        . . c b b b c d 5 5 b c . . . .
         """),
     SpriteKind.player)
-controller.move_sprite(my_sprite)
+Player_1.set_scale(0.6, ScaleAnchor.MIDDLE)
+controller.move_sprite(Player_1)
+tiles.set_current_tilemap(tilemap("""
+    level1
+    """))
+tiles.place_on_random_tile(Player_1, sprites.dungeon.stair_north)
+scene.camera_follow_sprite(Player_1)
+coconut_2 = 0
+
+def on_forever():
+    global coconut_2
+    info.change_score_by(1)
+    coconut_2 += 1
+    pause(1000)
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,19 +1,37 @@
-let my_sprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 2 2 . . . . . . . 
-    . . . . . . . 2 2 . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(my_sprite)
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
+    info.setScore(120 - 2 * coconut_2)
+    game.gameOver(true)
+})
+let coconut_2 = 0
+let Player_1 = sprites.create(img`
+        . . . . . . . . . . . . . . . .
+        . . . . c c c c . . . . . . . .
+        . . c c 5 5 5 5 c c . . . . . .
+        . c 5 5 5 5 5 5 5 5 c . . . . .
+        c 5 5 5 5 5 1 f 5 5 5 c . . . .
+        c 5 5 5 5 5 f f 5 5 5 5 c . . .
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . .
+        c c b b 1 b 5 5 5 5 5 5 d c . .
+        c 5 3 3 3 5 5 5 5 5 d d d c . .
+        . b 5 5 5 5 5 5 5 5 d d d c . .
+        . . c b b c 5 5 b d d d d c c .
+        . c b b c 5 5 b b d d d d c d c
+        . c c c c c c d d d d d d d d c
+        . . . c c c c d 5 5 b d d d c .
+        . . c c c c c b 5 5 b c c c . .
+        . . c b b b c d 5 5 b c . . . .
+        `, SpriteKind.Player)
+Player_1.setScale(0.6, ScaleAnchor.Middle)
+controller.moveSprite(Player_1)
+tiles.setCurrentTilemap(tilemap`
+    level1
+    `)
+tiles.placeOnRandomTile(Player_1, sprites.dungeon.stairNorth)
+scene.cameraFollowSprite(Player_1)
+coconut_2 = 0
+forever(function on_forever() {
+    
+    info.changeScoreBy(1)
+    coconut_2 += 1
+    pause(1000)
+})
