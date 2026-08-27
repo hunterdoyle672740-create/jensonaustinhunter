fd45257c8b0aae59699e637f478dc84f8931e17d
hunterdoyle672740-create <hunter.doyle672740@gmail.com>
2026-08-28 08:01:30 +1000

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+def on_a_pressed():
+    global lkfjadsflkj
+    lkfjadsflkj = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . 2 2 . . . . . . .
+            . . . . . . . f f . . . . . . .
+            . . . . . . . 2 2 . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            """),
+        Player_1,
+        50,
+        50)
+    lkfjadsflkj.follow(myEnemy)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
 def on_overlap_tile(sprite, location):
     info.set_score(120 - 2 * coconut_2)
     game.game_over(True)
@@ -5,7 +31,14 @@ scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.stair_large,
     on_overlap_tile)
 
+def on_on_overlap(sprite2, otherSprite):
+    game.game_over(False)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap)
+
+lkfjadsflkj: Sprite = None
 coconut_2 = 0
+myEnemy: Sprite = None
+Player_1: Sprite = None
 Player_1 = sprites.create(img("""
         . . . . . . . . . . . . . . . .
         . . . . c c c c . . . . . . . .
@@ -25,6 +58,35 @@ Player_1 = sprites.create(img("""
         . . c b b b c d 5 5 b c . . . .
         """),
     SpriteKind.player)
+myEnemy = sprites.create(img("""
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        ......fffcdb1bdffff.....
+        ....fc111cbfbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbffffffbfbfbf....
+        .........ffffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        """),
+    SpriteKind.enemy)
+myEnemy.follow(Player_1, 30)
+myEnemy.set_scale(0.4, ScaleAnchor.MIDDLE)
 Player_1.set_scale(0.6, ScaleAnchor.MIDDLE)
 controller.move_sprite(Player_1)
 tiles.set_current_tilemap(tilemap("""
@@ -33,6 +95,7 @@ tiles.set_current_tilemap(tilemap("""
 tiles.place_on_random_tile(Player_1, sprites.dungeon.stair_north)
 scene.camera_follow_sprite(Player_1)
 coconut_2 = 0
+myEnemy.set_position(160, 0)
 
 def on_forever():
     global coconut_2
